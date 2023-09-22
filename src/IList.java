90fc7ab6fa067ee480e3ea44508ea8ec1e50cc7c
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-09-22 10:16:43 -0400

    Fixes per lecture.

--- src/IList.java
+++ src/IList.java
@@ -3,4 +3,6 @@ package src;
 public interface IList {
     // All IList objects must have a size()
     int size();
+
+    Link addFirst(int newElt);
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,10 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-        IList l1 = new EmptyList();
-        IList l2 = new Link(5, l1);
-
-        IList l3 = new Link(5, new Link(42, new EmptyList()));
-
-        // L2 = L_immut.addFirst(4)
+            Link L_im1 = new EmptyList().addFirst(3).addFirst(5).addFirst(7);
+            Link L_im2 = L_im1.addFirst(4);
+            
+            System.out.println(L_im1);
+            System.out.println(L_im2);
     }
 }
